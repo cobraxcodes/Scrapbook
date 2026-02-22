01cb95d51c7c59fa5d7bf9a9ddbf26e04a7bae15
cobraxcodes <198425854+cobraxcodes@users.noreply.github.com>
2026-02-21 23:44:39 -0800

    complete test login and landing page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
   const navigate = useNavigate(); 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (username.trim() === "Admin") {
+    if (username.trim() === "Fatema") {
       navigate("/home");
     } else {
       alert("Boo 👎🏼👎🏼 You Cannot Enter!");

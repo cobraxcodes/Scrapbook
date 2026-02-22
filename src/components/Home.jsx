6a6d345680d24607f022492737b3cc6ccc453eb9
cobraxcodes <198425854+cobraxcodes@users.noreply.github.com>
2026-02-21 23:31:02 -0800

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1 className="logTitle">Welcome, My Love! 💖</h1>
+      <p className="para">It's not quite ready yet, come back later? XOXO</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../login.css";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate(); 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username.trim() === "Admin") {
+      navigate("/home");
+    } else {
+      alert("Boo 👎🏼👎🏼 You Cannot Enter!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="loginTitle">Hello 💖</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          className="inputBox"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <div>
+          <button className="loginBtn" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
